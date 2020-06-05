4e7c4d34619e03529bbb776eb7d20f0cff21786f
Brian Appleton <appleton6509@outlook.com>
2020-06-04 21:45:09 -0600

    Add project files.

---
+++ FPOSReports/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FPOSReports
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ItemSoldTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fpos5DataSet = new FPOSReports.fpos5DataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ItemSoldTableAdapter = new FPOSReports.fpos5DataSetTableAdapters.ItemSoldTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.ItemSoldTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpos5DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ItemSoldTableBindingSource
+            // 
+            this.ItemSoldTableBindingSource.DataMember = "ItemSoldTable";
+            this.ItemSoldTableBindingSource.DataSource = this.fpos5DataSet;
+            // 
+            // fpos5DataSet
+            // 
+            this.fpos5DataSet.DataSetName = "fpos5DataSet";
+            this.fpos5DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "ItemSoldDataSet";
+            reportDataSource1.Value = this.ItemSoldTableBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "FPOSReports.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(946, 694);
+            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // ItemSoldTableAdapter
+            // 
+            this.ItemSoldTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(946, 694);
+            this.Controls.Add(this.reportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FPOS Hourly Sales Report";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ItemSoldTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpos5DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private fpos5DataSet fpos5DataSet;
+        private fpos5DataSetTableAdapters.ItemSoldTableAdapter ItemSoldTableAdapter;
+        private System.Windows.Forms.BindingSource ItemSoldTableBindingSource;
+    }
+}
+
---
+++ FPOSReports/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FPOSReports
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("unable to connect to database");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // TODO: This line of code loads data into the 'fpos5DataSet.ItemSoldTable' table. You can move, or remove it, as needed.
+                this.ItemSoldTableAdapter.Fill(this.fpos5DataSet.ItemSoldTable);
+                this.reportViewer1.RefreshReport();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+#pragma warning disable IDE1006 // Naming Styles
+        private void reportViewer1_Load(object sender, EventArgs e)
+#pragma warning restore IDE1006 // Naming Styles
+        {
+
+        }
+    }
+}
